e07381891b3a03b53702a25d69b03aef519649fe
frahmadani <frahmadani@gmail.com>
2019-10-18 11:33:42 +0700

    add resize

--- components/resize/index.js
+++ components/resize/index.js
@@ -1,15 +1,19 @@
 const sharp = require('sharp')
 const {join} = require('path')
+const fs = require('fs')
 
-let originalImage = join(__dirname, 'tile6/combined.png')
-let outputImage = join(__dirname, 'tile6/cropped.png')
-
-sharp(originalImage).extract({ width: 3373, height: 3373, left: 0, top: 0}).toFile(outputImage)
-.then(function (newfile) {
-	console.log('saved')
-	return
-})
-.catch(function(err) {
-	console.log(err)
-	return
-})
+exports.resize = (dir, idx) => {
+	let originalImage = join(dir, '/stitched/', `temp_${idx+1}.png`)
+	let outputImage = join(dir, '/stitched/', `${idx+1}.png`)
+	
+	sharp(originalImage).extract({ width: 3373, height: 3373, left: 0, top: 0}).toFile(outputImage)
+	.then(function (newfile) {
+		console.log(`${idx+1}.png is cropped`)
+		fs.unlinkSync(`${originalImage}`)
+		return
+	})
+	.catch(function(err) {
+		console.log(err)
+		return
+	})
+}
--- index.js
+++ index.js
@@ -2,44 +2,48 @@
 
 const {join} = require('path')
 const combineTiles = require('./components/combine-tiles')
-// const resize = require('./components/resize')
+const resize = require('./components/resize')
 const fs = require('fs')
 
 const size = 512
 
-const source_image_dir = join(__dirname, 'images/tile1/')
+const images_dir = join(__dirname, 'images/')
+const tile_dirs = fs.readdirSync(images_dir)
 
-const tiles = []
-
-const files = fs.readdirSync(source_image_dir);
-
-let i = 0
-let j = 1
-
-files.forEach(file => {
-	if (i >= 7) {
-		i -= 7
-		j++
-		i++
-	} else {
-		i++
-	}
+tile_dirs.forEach((dir, index) => {
+	const tile_path = images_dir + dir
+	const tiles = []
+	const files = fs.readdirSync(tile_path)
 	
-	tiles.push({
-		x: i,
-		y: j,
-		file: join(source_image_dir, file)
+	let i = 0
+	let j = 1
+
+	files.forEach(file => {
+		if (i >= 7) {
+			i -= 7
+			j++
+			i++
+		} else {
+			i++
+		}
+		
+		tiles.push({
+			x: i,
+			y: j,
+			file: join(tile_path, file)
+		})
 	})
-})
 
-const dest = join(__dirname, 'stitched/1.png')
+	const dest = join(__dirname, 'stitched', `temp_${index+1}.png`)
 
-combineTiles(tiles, size, size, dest)
-// .then(() => {
-// 	resize()
-// 	process.exit(0)
-// })
-.catch((err) => {
-	console.error(err)
-	process.exit(1)
+	combineTiles(tiles, size, size, dest)
+	.then(() => {
+		console.log(`temp_${index+1}.png is created`)
+		resize.resize(__dirname, index)
+	})
+	.catch((err) => {
+		console.error(err)
+		process.exit(1)
+	})
+
 })
